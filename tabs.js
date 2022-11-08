69fa92b3aef7aeed315126ea9bb5baf4252bff49
Adam Hauck <ad.hauck@gmail.com>
2022-11-08 12:59:06 -0800

    set up keyboard navigation

---
+++ tabs.js
@@ -0,0 +1,15 @@
+const tabList = document.querySelector('[role="tablist"]');
+const tabs = tabList.querySelectorAll('[role="tab"]');
+
+tabList.addEventListener('keydown', (e) => {
+    const keydownLeft = 37;
+    const keydownRight = 39;
+
+    //when keydown, change tabindex of current tab to -1
+
+    //if the right key is pushed, move to the next tab on the right
+
+    //if the left key is pushed, move to the next tab on the left
+
+    
+})
