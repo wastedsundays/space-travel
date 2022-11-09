28840d9c1bbbea4ba94735460659312fbc481534
Adam Hauck <ad.hauck@gmail.com>
2022-11-08 16:41:49 -0800

    finished keyboard navigation of tabs

--- tabs.js
+++ tabs.js
@@ -1,15 +1,33 @@
 const tabList = document.querySelector('[role="tablist"]');
 const tabs = tabList.querySelectorAll('[role="tab"]');
 
-tabList.addEventListener('keydown', (e) => {
+tabList.addEventListener("keydown", changeTabFocus);
+
+let tabFocus = 0;
+
+function changeTabFocus(e) {
     const keydownLeft = 37;
     const keydownRight = 39;
-
-    //when keydown, change tabindex of current tab to -1
-
-    //if the right key is pushed, move to the next tab on the right
-
-    //if the left key is pushed, move to the next tab on the left
-
-    
-})
+  
+    if (e.keyCode === keydownLeft || e.keyCode === keydownRight) {
+      tabs[tabFocus].setAttribute("tabindex", -1);
+      // tabs[tabFocus].setAttribute("aria-selected", "false");
+    }
+  
+    if (e.keyCode === keydownRight) {
+      tabFocus++;
+      if (tabFocus >= tabs.length) {
+          tabFocus = 0;
+      }
+    }
+  
+    if (e.keyCode === keydownLeft) {
+      tabFocus--;
+      if (tabFocus < 0) {
+          tabFocus = tabs.length -1;
+      }
+    }
+  
+    tabs[tabFocus].setAttribute("tabindex",0);
+    tabs[tabFocus].focus();
+}
